8a538b844452e7d8f9efaea1c83470c1c9bb1043
MandyChadwick <cha33dwick@gmail.com>
2023-09-14 13:46:53 -0800

    prep 2 working

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percent = Console.ReadLine();
+        int intpercent = int.Parse(percent);
+
+        string letter = "";
+
+        if (intpercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (intpercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (intpercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (intpercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (intpercent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Just Keep Trying!");
+        }
     }
 }
