c551f2fe8511b4e7cc625a56d3e409298e1b8159
MandyChadwick <cha33dwick@gmail.com>
2023-09-15 11:01:46 -0800

    prep5 working

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+        Console.Write("Please enter your name ");
+        string yourName = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        string favoriteNumber = Console.ReadLine();
+
+        int intFavoriteNumber = int.Parse(favoriteNumber);
+        int square = SquareNumbers(intFavoriteNumber);
+        Console.Write($"{yourName}, the square of your number is {square}");
+
+
     }
+    static int SquareNumbers(int intFavoriteNumber)
+    {
+        int square = intFavoriteNumber * intFavoriteNumber;
+        return square;
+    }
+
 }
+
+
