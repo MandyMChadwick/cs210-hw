4961e0b447f4157147252cfb12173fd6acf3407e
MandyChadwick <cha33dwick@gmail.com>
2023-09-22 08:02:30 -0800

    Generator Code started

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,10 +1,70 @@
 public class PromptGenerator
 {
 
-    public List<string> _prompts;
-    public string GetRandomPrompt()
-    {
-        return "";
-    }
+    string[] prompts = new string[]
 
+{
+
+        "What are you grateful for today?",
+
+        "What are you looking forward to tomorrow?",
+
+        "What is something you learned today?",
+
+        "What is something you want to improve on?",
+
+        "What is something that made you happy today?"
+
+};
+
+
+
+    public string GetRandomPrompt()
+
+    {
+
+        Random rnd = new Random();
+
+        int index = rnd.Next(prompts.Length); // Generate a random index
+
+        return prompts[index]; // Return the prompt at that index
+
+    }
+    /*
+     Video Example
+     public List<string> _prompts;
+      public string GetRandomPrompt()
+      {
+          return "";
+      }
+
+     */
+
+
+
+    /* Possible approach
+    public class GetRandomPrompt
+    {
+        public List<string> _prompts;
+
+        public GetRandomPrompt()
+        {
+            _prompts = new List<string>
+            {
+                "What are you grateful for today?",
+                "What are you looking forward to tomorrow?",
+                "What is something you learned today?",
+                "What is something you want to improve on?",
+                "What is something that made you happy today?"
+            };
+        }
+
+        public string GetRandom()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(_prompts.Count); // Generate a random index
+            return _prompts[index]; // Return the prompt at that index
+        }
+    }
+    */
 }
