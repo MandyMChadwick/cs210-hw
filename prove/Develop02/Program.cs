0af56abc83b060cf986b6aaf406e794d3b25e622
MandyChadwick <cha33dwick@gmail.com>
2023-09-22 07:18:56 -0800

    sandbox/start of journal where loop and quit work

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        bool running = true; // a boolean variable to control the loop
+        while (running) // a while loop that runs until running is false
+        {
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            string response = Console.ReadLine();
+            int intResponse = int.Parse(response);
+            if (intResponse == 1)
+            {
+                // Write.Writing();
+                Console.WriteLine("You Chose 1");
+            }
+            else if (intResponse == 2)
+            {
+                //  Displaying.Display();
+                Console.WriteLine("You Chose 2");
+            }
+            else if (intResponse == 3)
+            {
+                Console.WriteLine("You Chose 3");
+            }
+            else if (intResponse == 4)
+            {
+                Console.WriteLine("You Chose 4");
+            }
+            else if (intResponse == 5)
+            {
+                Console.WriteLine("You Chose 5");
+                running = false; // set running to false to exit the loop
+            }
+            else
+            {
+                Console.WriteLine("You didn't choose a number between 1-5");
+            }
+        }
     }
 }
---
+++ sandbox/Sandbox/Person.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Person
+{
+    public string _firstName;
+    public string _lastName;
+    public int _age;
+
+}
