2a10001e0b3a30da017163cce833e993d5343ba7
MandyChadwick <cha33dwick@gmail.com>
2023-10-02 10:43:18 -0800

    assignment class added to learning04

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,70 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+
+    }
+    public string getStudentName()
+    {
+        return _studentName;
+
+    }
+
+    public string getTopic()
+
+    {
+        return _topic;
+
+    }
+
+
+
+
+    string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+
+
+
+
+
+    /*public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+}*/
+
+
+
+
+
+
+}
+
