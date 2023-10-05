ea29d2d4e8504a64a98ee4524e30c3a3869262c8
MandyChadwick <cha33dwick@gmail.com>
2023-10-05 12:59:45 -0800

    menu in place and activity place holders

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,34 @@
+class Activity
+{
+    private string _string;
+    private string _description;
+    private int _duration;
+
+    Activity()
+    {
+
+    }
+
+    public void DisplayStartingMessage()
+    {
+
+    }
+
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+
+    }
+
+
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,39 @@
 using System;
-
 class Program
 {
+    // Simple menu to test out the game classes.
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        string choice = "0";
+        do
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start breathing activity");
+            Console.WriteLine("2. Start reflecting activity");
+            Console.WriteLine("3. Start listing activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Your choice from the menu: ");
+            choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                Console.WriteLine("you chose the breathing activity");
+                //GuessingGame g = new GuessingGame();
+                //g.Play();
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("you chose the reflecting activity");
+                // CodeWordGame g = new CodeWordGame();
+                // g.Play();
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("you chose the listing activity");
+                // MathGame g = new MathGame();
+                //g.Play();
+            }
+        }
+        while (choice != "4");
+        Console.WriteLine("Goodbye!");
     }
 }
