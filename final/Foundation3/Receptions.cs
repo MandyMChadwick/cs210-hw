96ddad8e77e6c20f6e22ad72779676046b7d654d
MandyChadwick <cha33dwick@gmail.com>
2023-10-16 08:29:38 -0800

    removed space on rsvp

--- final/Foundation3/Receptions.cs
+++ final/Foundation3/Receptions.cs
@@ -17,6 +17,6 @@ public class Receptions : Event
 
     public override string DisplayFullDetail()
     {
-        return base.DisplayFullDetail() + $"\n RSVP at: {_email}";
+        return base.DisplayFullDetail() + $"\nRSVP at: {_email}";
     }
 }
