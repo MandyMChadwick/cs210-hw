1c92508b3d65803b205c0baf11850175c8d68444
MandyChadwick <cha33dwick@gmail.com>
2023-11-03 07:42:08 -0800

    update

--- sandbox/Sandbox/fileSearch.cs
+++ sandbox/Sandbox/fileSearch.cs
@@ -3,75 +3,36 @@
 @page
 @model IndexModel
 
-<h1>File List</h1>
+<select id="idList">
+    <option value="1">ID 1</option>
+    <option value="2">ID 2</option>
+    <option value="3">ID 3</option>
+    <!-- Add more options as needed -->
+</select>
 
-<ul>
-    @foreach (var file in Model.Files)
-    {
-        <li>
-            <a href="javascript:void(0)" class="file-link" data-file="@file">@file</a>
-        </li>
-    }
-</ul>
+<button id="getDataButton">Get Data</button>
 
-<div id="file-details"></div>
+<div id="result"></div>
 
-@section scripts {
-    <script>
-        $(document).ready(function () {
-            $('.file-link').click(function () {
-                var fileName = $(this).data('file');
-                $.ajax({
-                    type: 'GET',
-                    url: '/Index?handler=GetFileDetails',
-                    data: { fileName: fileName },
-                    success: function (data) {
-                        $('#file-details').html(data);
-                    }
-                });
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#getDataButton').click(function () {
+            var selectedId = $('#idList').val(); // Get the selected ID from the list
+
+            $.ajax({
+                type: 'GET',
+                url: '/GetData', // Replace with your endpoint
+                data: { id: selectedId }, // Pass the selected ID in the AJAX request
+                success: function (data) {
+                    $('#result').html(data); // Display the received data in a result container
+                },
+                error: function () {
+                    $('#result').html('Error fetching data.'); // Display an error message if the AJAX call fails
+                }
             });
         });
-    </script>
-}
+    });
+</script>
 
-
-
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
-
-namespace FileListApp.Pages
-{
-    public class IndexModel : PageModel
-    {
-        public List<string> Files { get; set; }
-
-        public async Task OnGet()
-        {
-            // Replace with the path to your folder
-            var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "YourFolderPath");
-
-            Files = Directory.GetFiles(folderPath)
-                .Select(Path.GetFileName)
-                .ToList();
-        }
-
-        public IActionResult OnGetGetFileDetails(string fileName)
-        {
-            // Replace this with code to retrieve information from the database based on the fileName
-            var fileDetails = GetFileDetailsFromDatabase(fileName);
-
-            return Content(fileDetails);
-        }
-
-        private string GetFileDetailsFromDatabase(string fileName)
-        {
-            // Replace this with your database retrieval logic
-            return $"Details for {fileName} from the database.";
-        }
-    }
-}
 */
